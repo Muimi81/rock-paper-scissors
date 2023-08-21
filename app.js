de2a259f829955c4801cb5257c982fa3feb51ab8
Nzangi Muimi <nzangimuimi@gmail.com>
2023-08-21 12:44:24 +0300

    declare constants

---
+++ app.js
@@ -0,0 +1,8 @@
+//js file for my Rock Paper Scissors Project//
+
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
