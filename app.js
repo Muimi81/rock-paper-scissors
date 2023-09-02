8ac137b9578f7928f31d102fd2c2c3f905457b34
Nzangi Muimi <nzangimuimi@gmail.com>
2023-09-02 20:56:03 +0300

    Add fucntion win, lose and draw.

--- app.js
+++ app.js
@@ -7,13 +7,25 @@ const computerScore_span = document.getElementById ("computer-score");
 const scoreBoard_div = document.querySelector (".score-board");
 const result_div = document.querySelector (".result");
 const rock_div = document.getElementById ("rock");
-const paper_div = documnt.getElementById ("paper");
+const paper_div = document.getElementById ("paper");
 const scissors_div = document.getElementById ("scissors");
 
 function getComputerChoice (){
     const choices = ['rock', 'paper', 'scissors'];
     const randomNumber = Math.floor(Math.random()*3);
     return choices[randomNumber];
+}
+
+function win() {
+    console.log("USER WINS");
+}
+
+function lose() {
+    console.log("USER LOST");
+}
+
+function draw() {
+    console.log("DRAW");
 }
 
 function game(userChoice) {
